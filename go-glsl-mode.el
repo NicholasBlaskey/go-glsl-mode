2f0912bcc08e7ed3c4f0a8f58eee4fae8bc31817
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2021-09-29 19:35:59 -0400

    Made little progress but saving for refrence

--- go-glsl-mode.el
+++ go-glsl-mode.el
@@ -3,7 +3,6 @@
 (require 'glsl-mode) ;; for syntax hightlighting
 
 
-
 ;(message glsl-font-lock-keywords-1)
 
 ; https://emacs.stackexchange.com/questions/10165/can-i-add-highlighting-in-a-minor-mode
@@ -15,7 +14,6 @@
 ;(message "My list is %s" sql-mode-ansi-font-lock-keywords)
 
 
-
 (defun go-sql-keyword-matcher (end)
   "Search for SQl keywords within Go strings."
   (let (pos (case-fold-search t))
@@ -23,6 +21,15 @@
                 (null (nth 3 (syntax-ppss pos)))))
     (when pos (message "Found pos keyword at %s" pos))
     pos))
+
+
+;(defun go-sql-keyword-matcher (end, keywords)
+;  "Search for SQl keywords within Go strings."
+;  (let (pos (case-fold-search t))
+;    (while (and (setq pos (re-search-forward keywords end t))
+;                (null (nth 3 (syntax-ppss pos)))))
+;    (when pos (message "Found pos keyword at %s" pos))
+;    pos))
 
 
 (defcustom go-sql-keyword-face 'go-sql-keyword-face
@@ -37,22 +44,29 @@
    '((go-sql-keyword-matcher 0 glsl-type-face t))
    'append))
 
+;(defun add-single-highlighter (val)
+;  (message (nth 0 val)))
+;  (font-lock-add-keywords
+;   nil
+;   '((go-sql-keyword-matcher 0 (nth 1 val) (nth 0 val) t))
+;   'append))
 
-(defun message2 (val)
-  (message (nth 0 val))
- )
+(defun add-highlighter ()
+  (mapcar 'add-single-highlighter glsl-font-lock-keywords-1)
+  )
   
-(mapcar 'message2 glsl-font-lock-keywords-1)
-
-(defun test ()
-  (message hello)
-)
+;(mapcar 'message2 glsl-font-lock-keywords-1)
   
-;(remove-hook 'go-mode-hook 'go-add-sql-keyword-matcher)
-;(add-hook 'go-mode-hook 'go-add-sql-keyword-matcher)
+(remove-hook 'go-mode-hook 'go-add-sql-keyword-matcher)
+(add-hook 'go-mode-hook 'go-add-sql-keyword-matcher)
 
-(remove-hook 'go-mode-hook 'test)
-(add-hook 'go-mode-hook 'test)
+;(remove-hook 'go-mode-hook 'add-highlighter)
+;(add-hook 'go-mode-hook 'add-highlighter)
+
+
+
+;(remove-hook 'go-mode-hook 'test)
+;(add-hook 'go-mode-hook 'test)
 
 
 ;(define-minor-mode blah-mode
--- test.go
+++ test.go
@@ -1,3 +1,4 @@
+// ehy
 package main
 
 import (

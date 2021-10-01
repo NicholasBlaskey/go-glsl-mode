ff8891706645c9d752be8f788580406a385749ec
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2021-09-30 20:06:55 -0400

    Got changing faces working

--- go-glsl-mode.el
+++ go-glsl-mode.el
@@ -5,17 +5,19 @@
 (defun match-regexes (end)
   "Search for SQl keywords within Go strings."
   (let (pos (case-fold-search t))
-    (while (and (setq pos (re-search-forward (nth 0 (nth 0 glsl-font-lock-keywords-1)) end t))
+    (while (and (setq pos (re-search-forward (nth 0 (nth 4 glsl-font-lock-keywords-1)) end t))
                 (null (nth 3 (syntax-ppss pos)))))
     (when pos (message "Found pos keyword at %s" pos))
     pos))
 
 
+;(message (nth 1 (nth 4 glsl-font-lock-keywords-1)))
+
 (defun go-add-sql-keyword-matcher ()
   "Hook to add fontification of sql-keywords in strings."
   (font-lock-add-keywords
    nil
-   '((match-regexes 0 glsl-type-face t))
+   '((match-regexes 0 (nth 4 glsl-font-lock-keywords-1) t))
    'append))
 
 (remove-hook 'go-mode-hook 'go-add-sql-keyword-matcher)
